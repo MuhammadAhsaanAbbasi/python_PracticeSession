836a96d31db1352b349e0069b8a5389c0a082f5a
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-02-23 23:28:19 +0500

    init dictionary

--- class6/class6.py
+++ class6/class6.py
@@ -11,3 +11,16 @@ profile: Dict[key, value] = {
 }
 
 pprint.pprint(profile)
+
+favorite_languages: Dict[key, value] = {
+                                        "jen": "python",
+                                        "sarah": "c",
+                                        "edward": "ruby",
+                                        "phil": "python",
+}
+people = ["jen", "sarah", "edward", "phil", "ahmed", "ali", "hamza"]
+for person in people:
+    if person in favorite_languages.keys():
+        print(f"Thanks for responding {person}\n")
+    else:
+        print(f"{person} please take the poll\n")
