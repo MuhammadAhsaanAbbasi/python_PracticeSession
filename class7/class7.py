41ff254b2987912d6b517d653147d537b425dcfd
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-02-28 12:41:42 +0500

    file 7

--- class7/class7.py
+++ class7/class7.py
@@ -3,3 +3,13 @@ import sys
 print("Hello world!")
 
 print("This is the name of the script: ", sys.argv)
+
+while True:
+    age = int(input("Enter your age"))
+    quit: str = input("Enter quit to exit")
+    if age < 3:
+        print("The ticket is free")
+    elif age >= 3 and age <= 12:
+        print("The ticket is $10")
+    else:
+        print("The ticket is $15")
