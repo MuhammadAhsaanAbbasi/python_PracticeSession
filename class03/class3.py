705e62f6bc4c4d36c4b2e24d791e9acf53eaa44e
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-02-28 12:33:39 +0500

    third file

--- class03/class3.py
+++ class03/class3.py
@@ -1,1 +1,21 @@
 print("classo3")
+
+mountains: list[str] = ["K2","Everest","Nanga Parbat","Himalaya"]
+print(mountains)
+print(sorted(mountains))
+print(mountains.pop())
+mountains[0] = "Karakoram"
+print(mountains)
+mountains.reverse()
+print(mountains)
+mountains.sort()
+print(mountains)
+mountains.append("Himalaya")
+print(mountains)
+mountains.insert(0,"Hindukush")
+print(mountains)
+mountains.remove("Himalaya")
+print(mountains)
+mountains2: list[str] = ["K2","Himalaya","Tirich Mir"]
+mountains.extend(mountains2)
+print(mountains)
