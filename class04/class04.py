d4212321a92df678dd72bcc1c91328851de9d1b3
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-02-28 12:36:59 +0500

    forth file

--- class04/class04.py
+++ class04/class04.py
@@ -1,1 +1,10 @@
 print("Hello, World!")
+pizzas = ['pepperoni', 'margherita', 'hawaiian', 'veggie', 'cheese']
+for i in pizzas[0:3]:
+    print(f"The first three items in the list are: {i}\n")
+
+for i in pizzas[1:4]:
+    print(f"Three items from the middle of the list are: {i}\n")
+
+for i in pizzas[2:5]:
+    print(f"The last three items in the list are: {i}\n")
