b877a13d9a0d9c2a918838ec2449de0927280008
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-02-28 12:32:43 +0500

    second file

--- class02/class2.py
+++ class02/class2.py
@@ -1,2 +1,9 @@
 name: str = "Hijabie"
 print(name)
+
+names: list[str] = ["Muhammad Ahsaan", "Hijabie Rabail", "Riv Khan"]
+uinput: str = input("Enter your name: ")
+if uinput in names:
+    print(uinput, "is in the list")
+elif uinput not in names:
+    print(uinput, "is not in the list")
