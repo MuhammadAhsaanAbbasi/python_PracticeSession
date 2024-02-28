9de2cc94f4af6ec08d9ae7c4f1e51b83e8bb972e
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-02-28 12:31:53 +0500

    first file

--- class01/class1.py
+++ class01/class1.py
@@ -3,3 +3,11 @@ print('Hijabie')
 name: str = "Hijabie"
 name = "Rawa"
 print(name)
+
+author2: str = "Mirza Ghalib"
+quote2: str = """In countless wounds, many a life departed with each pain,
+In every agony, many a soul departed again"""
+message2: str = f""" 
+{author2} once said,
+"{quote2}" """
+print(message2)
